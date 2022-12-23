1bdf1e8cbd987e1a3c1841b1243ec32dcbe08441
Samipthi Nayak <samipthidnayak@gmail.com>
2022-12-23 18:50:19 +0530

    update
    modified

--- ui.R
+++ ui.R
@@ -1,7 +1,13 @@
 library(shiny)
 
-ui <- fluidPage(
-  selectInput("dataset", label = "Dataset", choices = ls("package:datasets")),
-  verbatimTextOutput("summary"),
-  tableOutput("table")
-)
+library(leaflet)
+
+
+navbarPage("Location of Blood Banks", id="main",
+           theme = bslib::bs_theme(bootswatch = "darkly"),
+           tabPanel("Map", leafletOutput("bbmap", height=1000)),
+           tabPanel("Data", DT::dataTableOutput("data")),
+           tabPanel("graphs",titlePanel("Hospital Anaysis"),
+                    # Bar Chart
+                    plotOutput("brandBar")),
+           tabPanel("Read Me",includeMarkdown("readme.md")))
